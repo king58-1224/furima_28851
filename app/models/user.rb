330ffe3f05b6cad130e5d07e9a9a8e876813f374
king58-1224 <king581224@gmail.com>
2020-09-05 18:34:57 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true,format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    it 'すべての値が正しく入力されていれば保存できること' do
+      expect(@user).to be_valid
+    end
+    it "ニックネームが必須であること" do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
+    it "メールアドレスが必須であること" do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+    it "メールアドレスが一意性であること" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+    end
+    it "メールアドレスは@を含む必要があること" do
+      @user.email = "xxxxxx@@@.co.jp"
+      @user.valid?
+      expect(@user.errors.full_messages[0]).to include("is invalid")
+    end
+    it "パスワードが必須であること" do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+    it "パスワードは6文字以上であること" do
+      @user.password = "00000"
+      @user.password_confirmation = "00000"
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+    it "パスワードは半角英数字混合であること" do
+      @user.password = "12345678"
+      @user.valid?
+      expect(@user.errors.full_messages[0]).to include("is invalid")
+    end
+    it "パスワードは確認用を含めて2回入力すること" do
+      @user.password_confirmation = ""
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+  end
+end
