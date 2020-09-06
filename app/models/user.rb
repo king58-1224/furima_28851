2cb48d8b45ece7e8ee748b7a939be0ef1693020c
king58-1224 <king581224@gmail.com>
2020-09-06 15:01:32 +0900

    エラーメッセージ表示の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true,format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+  validates :email, presence: true,format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: "Email can't be blank."}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Password can't be blank." }
+  validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "First name is invalid. Input full-width characters."}
+  validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "First name kana is invalid. Input full-width katakana characters.."}
+  validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "Family name is invalid. Input full-width characters."}
+  validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "Family name kana is invalid. Input full-width katakana characters."}
   validates :birthday, presence: true
 end
 
