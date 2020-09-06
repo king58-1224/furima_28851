e23f93e5ea06ba6da3ebfd2416ca3f50ba6a9f1c
king58-1224 <king581224@gmail.com>
2020-09-06 16:23:05 +0900

    テストコード修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,34 +44,43 @@ RSpec.describe User, type: :model do
     it "パスワードは半角英数字混合であること" do
       @user.password = "12345678"
       @user.valid?
-      expect(@user.errors.full_messages[0]).to include("is invalid")
+      
+      expect(@user.errors.full_messages[0]).to include("Password Password can't be blank.")
     end
     it "パスワードは確認用を含めて2回入力すること" do
       @user.password_confirmation = ""
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
-    it "ユーザー本名が、名字と名前がそれぞれ必須であること" do
+    it "ユーザー本名の名字が必須であること" do
       @user.first_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name First name is invalid. Input full-width characters.")
+    end
+    it "ユーザー本名の名前が必須であること" do
       @user.family_name = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")
+      expect(@user.errors.full_messages).to include("Family name Family name is invalid. Input full-width characters.")
     end
     it "ユーザー本名は全角（漢字・ひらがな・カタカナ）で入力させること" do
       @user.first_name = "king"
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name is invalid. Input full-width characters.")
+      expect(@user.errors.full_messages).to include("First name First name is invalid. Input full-width characters.")
     end
-    it "ユーザー本名のフリガナが、名字と名前でそれぞれ必須であること" do
+    it "ユーザー本名のフリガナの名字が必須であること" do
       @user.first_name_kana = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana First name kana is invalid. Input full-width katakana characters..")
+    end
+    it "ユーザー本名のフリガナの名前必須であること" do
       @user.family_name_kana = nil
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name kana is invalid. Input full-width katakana characters.")
+      expect(@user.errors.full_messages).to include("Family name kana Family name kana is invalid. Input full-width katakana characters.")
     end
     it "ユーザー本名のフリガナは全角（カタカナ）で入力させること" do
       @user.first_name_kana = "king"
       @user.valid?
-      expect(@user.errors.full_messages).to include("First name kana is invalid. Input full-width katakana characters.")
+      expect(@user.errors.full_messages).to include("First name kana First name kana is invalid. Input full-width katakana characters..")
     end
     it "生年月日が必須であること" do
       @user.birthday = nil
