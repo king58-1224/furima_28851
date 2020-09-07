96491fadb42695ca189eca0951a56663f5843d55
king58-1224 <king581224@gmail.com>
2020-09-07 20:55:23 +0900

    ハッシュ機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,4 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :shipping_cost,:shipping_days, :shipping_from
 end
